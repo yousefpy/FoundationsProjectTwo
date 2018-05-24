3e1f0b69269b773973774a87e34c6d3953be9a87
sentient64 <m.alsharekh94@gmail.com>
2018-05-24 17:05:52 +0300

    Added goback feature

--- view.py
+++ view.py
@@ -2,24 +2,27 @@
 from data import stores, print_stores
 from model import Cart
 
-print_stores()
-store_name = input("Pick a store by typing its name.\n")
-picked_store = None
-for store in stores:
-    if store.name.lower() == store_name.lower():
-        picked_store = store
-
-for product in picked_store.products:
-    print(product)
-    print()
-
 cart = Cart()
-item = ""
-item = input()
-while item.lower() != "checkout":
+while True:
+    item = ""
+    print_stores()
+    store_name = input("Pick a store by typing its name.\n")
+    picked_store = None
+    for store in stores:
+        if store.name.lower() == store_name.lower():
+            picked_store = store
+
     for product in picked_store.products:
-        if item.lower() == product.name.lower():
-            cart.add_to_cart(product)
-    item = input()
+        print(product)
+        print()
+
+    while item != "go back" and item != "checkout":
+        for product in picked_store.products:
+            if item.lower() == product.name.lower():
+                cart.add_to_cart(product)
+        item = input()
+
+    if item == "checkout":
+        break
 
 cart.checkout()
