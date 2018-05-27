5a9fce361157d59b98b4d0fbf159da9dec1a291e
sentient64 <m.alsharekh94@gmail.com>
2018-05-27 16:01:56 +0300

    proper formatting of prints, and made UI functionality into functions

--- model.py
+++ model.py
@@ -48,3 +48,43 @@ class Cart():
             print("Your order has been placed.")
         else:
             print("Your order has been cancelled.")
+
+
+site_name = "www.achoo.com"
+cart = Cart()
+
+def welcome():
+    print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % site_name)
+
+def shop():
+    item = ""
+    while item.lower() != "checkout":
+        item = ""
+        print_stores()
+        store_name = input("Pick a store by typing its name. Or type \"checkout\" to pay your bills and say your goodbyes.\n")
+        picked_store = None
+
+        # make util function
+        for store in stores:
+            if store.name.lower() == store_name.lower():
+                picked_store = store
+
+        # make method
+        print("-----------------------------------")
+        print("%s:" % picked_store.name)
+        for product in picked_store.products:
+            print(product)
+            print()
+
+        print("Pick the items you'd like to add in your cart by typing the product name exactly as it was spelled above.")
+        print("Type \"checkout\" to pay your bills and say your goodbyes.")
+        print("Type \"back\" to go back and shop at another store.")
+
+        while item.lower() != "back" and item.lower() != "checkout":
+            for product in picked_store.products:
+                if item.lower() == product.name.lower():
+                    cart.add_to_cart(product)
+            item = input()
+
+def thank_you():
+    print("Thank you for shopping with us at %s" % site_name)
--- view.py
+++ view.py
@@ -1,54 +1,11 @@
 # TRIGGERING USER INTERFACE FUNCTIONALITY
 from data import stores, print_stores
-from model import Cart
+from model import Cart, welcome, shop, cart, thank_you
 
-site_name = "www.achoo.com"
-cart = Cart()
 
-def welcome():
-    print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % site_name)
-
-def shop():
-    item = ""
-    while item.lower() != "checkout":
-        item = ""
-        print_stores()
-        store_name = input("Pick a store by typing its name. Or type \"checkout\" to pay your bills and say your goodbyes.\n")
-        picked_store = None
-
-        # make util function
-        for store in stores:
-            if store.name.lower() == store_name.lower():
-                picked_store = store
-
-        # make method
-        print("-----------------------------------")
-        print("%s:" % picked_store.name)
-        for product in picked_store.products:
-            print(product)
-            print()
-
-        print("Pick the items you'd like to add in your cart by typing the product name exactly as it was spelled above.")
-        print("Type \"checkout\" to pay your bills and say your goodbyes.")
-        print("Type \"back\" to go back and shop at another store.")
-
-        while item.lower() != "back" and item.lower() != "checkout":
-            for product in picked_store.products:
-                if item.lower() == product.name.lower():
-                    cart.add_to_cart(product)
-            item = input()
-
-def thank_you():
-    print("Thank you for shopping with us at %s" % site_name)
 
 
 welcome()
 shop()
 cart.checkout()
 thank_you()
-
-
-# welcome()
-# shop()  # repeats until "checkout"
-# checkout()
-# thank_you()
