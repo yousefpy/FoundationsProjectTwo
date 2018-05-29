61b2dae5a584bbe5e28a4f96eb208ab8eced30aa
sentient64 <m.alsharekh94@gmail.com>
2018-05-29 15:55:24 +0300

    Removed code for student to write.

--- model.py
+++ model.py
@@ -1,58 +1,35 @@
 # CLASSES AND METHODS
 class Store():
     def __init__(self, name):
-        self.name = name
-        self.products = []
+        # your code goes here!
 
     def add_product(self, product):
-        self.products.append(product)
+        # your code goes here!
 
     def print_products(self):
-        print("-----------------------------------")
-        print("%s:" % self.name)
-        for product in self.products:
-            print(product)
-            print()
+        # your code goes here!
 
 
 class Product():
     def __init__(self, name, description, price):
-        self.name = name
-        self.description = description
-        self.price = price
+        # your code goes here!
 
     def __repr__(self):
-        return "\tProduct Name: %s\n\tDescription: %s\n\tPrice: %s KWD" %(self.name, self.description, self.price)
+        # your code goes here!
 
 
 class Cart():
     def __init__(self):
-        self.products = []
+        # your code goes here!
 
     def add_to_cart(self, product):
-        self.products.append(product)
+        # your code goes here!
 
     def get_total_price(self):
-        price = 0
-        for product in self.products:
-            price += product.price
-
-        return price
+        # your code goes here!
 
     def print_receipt(self):
-        print("Here's your receipt: ")
-        for product in self.products:
-            print(product)
-            print()
-
-        print("Your total price is: KD%s" % self.get_total_price())
+        # your code goes here!
 
     def checkout(self):
-        print("-----------------------------------")
-        self.print_receipt()
-        confirm = input("Confirm?(yes/no)")
-        if confirm.lower() == "yes":
-            self.products = []
-            print("Your order has been placed.")
-        else:
-            print("Your order has been cancelled.")
+        # your code goes here!
