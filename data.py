594204017628be273849fffb0c57ae832dd98bf7
sentient64 <m.alsharekh94@gmail.com>
2018-05-30 14:10:02 +0300

    Copied changes to data.py from solution branch

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # CREATION OF DATA
-####################### DO NOT MODIFY THE CODE BELOW ########################
+######################### DO NOT MODIFY THIS CODE ###########################
 from model import Store, Product
 
 stores = []
@@ -10,13 +10,25 @@ def create_product(store, name, description, price):
 zara = Store("Zara")
 nike = Store("Nike")
 apple = Store("Apple")
-stores = [zara, nike, apple]
-
+stores.append(zara)
+stores.append(nike)
+stores.append(apple)
 create_product(zara, "Nice Yellow Top", "A nice yellow top.", 30)
 create_product(zara, "Pants", "100 percent cotton", 25)
-
 create_product(nike, "Running Shoes", "Shoes for running.", 15)
 create_product(nike, "Style Shoes", "Stylish shoes.", 20)
-
 create_product(apple, "iPhone X", "256GB Space Gray", 310)
 create_product(apple, "iPhone 5s", "128GB Silver", 250)
+
+#############################################################################
+# To create a new store:
+    # Create a new Store() object, and append it to the stores list.
+
+# To add a new product to the store:
+    # use the create_product() function
+    # which takes the store you want to add the product to, and the product details
+
+pharmacy = Store("Pharmacy")
+stores.append(pharmacy)
+create_product(pharmacy, "Melatonin 5mg", "50 pills", 5)
+create_product(pharmacy, "Melatonin 10mg", "50 pills", 8)
