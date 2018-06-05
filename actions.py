d00d6770b65393285fc2b249f59aa2d8ea4348c9
sentient64 <m.alsharekh94@gmail.com>
2018-06-05 17:12:23 +0300

    Added docstrings

--- actions.py
+++ actions.py
@@ -8,18 +8,33 @@ def welcome():
     print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % site_name)
 
 def print_stores():
+    """
+    prints the list of stores in a nice readable format.
+    """
     # your code goes here!
 
 def get_store(store_name):
+    """
+    receives a name for a store, and returns the store object with that name.
+    """
     # your code goes here!
 
 def pick_store():
+    """
+    prints list of stores and prompts user to pick a store.
+    """
     # your code goes here!
 
 def pick_products(cart, picked_store):
+    """
+    prints list of products and prompts user to add products to card.
+    """
     # your code goes here!
 
 def shop():
+    """
+    The main shopping functionality
+    """
     # your code goes here!
 
 def thank_you():
--- components.py
+++ components.py
@@ -1,17 +1,29 @@
 # CLASSES AND METHODS
 class Store():
     def __init__(self, name):
+        """
+        Initializes a new store with a name.
+        """
         # your code goes here!
 
     def add_product(self, product):
+        """
+        Adds a product to the list of products in this store.
+        """
         # your code goes here!
 
     def print_products(self):
+        """
+        Prints all the products of this store in a nice readable format.
+        """
         # your code goes here!
 
 
 class Product():
     def __init__(self, name, description, price):
+        """
+        Initializes a new product with a name, a description, and a price.
+        """
         # your code goes here!
 
     def __repr__(self):
@@ -20,16 +32,31 @@ class Product():
 
 class Cart():
     def __init__(self):
+        """
+        Initializes a new cart with an empty list of products.
+        """
         # your code goes here!
 
     def add_to_cart(self, product):
+        """
+        Adds a product to this cart.
+        """
         # your code goes here!
 
     def get_total_price(self):
+        """
+        Returns the total price of all the products in this cart.
+        """
         # your code goes here!
 
     def print_receipt(self):
+        """
+        Prints the receipt in a nice readable format.
+        """
         # your code goes here!
 
     def checkout(self):
+        """
+        Does the checkout.
+        """
         # your code goes here!
